e63e7fd7bc36b51ec0865ebc5916c0875b577344
bFlood <bFlood@spatialdatalogic.com>
2015-03-04 10:47:34 -0500

    Update Notification.java

--- src/android/am/armsoft/plugins/Notification.java
+++ src/android/am/armsoft/plugins/Notification.java
@@ -180,6 +180,8 @@ public class Notification extends CordovaPlugin {
                 });
 
                 //changeTextDirection(dlg);
+               dlg.create();
+               dlg.show();
             };
         };
         this.cordova.getActivity().runOnUiThread(runnable);
